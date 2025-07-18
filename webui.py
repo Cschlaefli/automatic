01205b4fb26a04db620acba0ac1caa41b0847d60
Cade Schlaefli <cade.schlaefli@gmail.com>
2025-07-18 17:59:02 -0500

    fix: ensure base reqs before api run

--- webui.py
+++ webui.py
@@ -19,7 +19,7 @@ otel_setup() # Avoid modules overriding otel log format
 import modules.loader
 import modules.hashes
 
-from installer import log, git_commit, custom_excepthook, set_log_levels, set_environment, install_extensions
+from installer import log, git_commit, custom_excepthook, set_log_levels, set_environment, install_extensions, ensure_base_requirements
 from modules import timer, paths, shared, extensions, gr_tempdir, modelloader, modeldata
 from modules.call_queue import queue_lock, wrap_queued_call, wrap_gradio_gpu_call # pylint: disable=unused-import
 import modules.devices
@@ -426,6 +426,7 @@ def webui(restart=False):
 @tracer.start_as_current_span("api_only")
 def api_only():
     set_log_levels()
+    ensure_base_requirements() # for pkg_resources, fix this
     set_environment()
     install_extensions()
     start_common()
