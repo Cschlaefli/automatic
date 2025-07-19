b57f9d38c47d81b7001e9e9fbd44d9a994334e13
Cade Schlaefli <cade.schlaefli@gmail.com>
2025-07-19 13:53:14 -0500

    move cuda device up

--- webui.py
+++ webui.py
@@ -425,6 +425,7 @@ def mount_subpath(app):
 
 @tracer.start_as_current_span("api_only")
 def api_only():
+    verify_cuda_device()
     set_log_levels()
     set_environment()
     container_install_extensions()
@@ -454,7 +455,6 @@ def verify_cuda_device():
 
 
 def start():
-    verify_cuda_device()
     api = api_only()
     uvicorn.run(
         app=api.app,
